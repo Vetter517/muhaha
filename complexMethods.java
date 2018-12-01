65e1ff7048cbb9809953618ae66228951ed771a0
korsunator <35782970+korsunator@users.noreply.github.com>
2018-11-30 23:52:57 -0500

    Update complexMethods.java

--- complexMethods.java
+++ complexMethods.java
@@ -55,15 +55,47 @@ public class complexMethods {
 		}
 	}//end release
 	
+	public static void movement(room[] r) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("1. Move a patient to Surgery");
+		System.out.println("2. Remove a patient from Surgery");
+		System.out.println("3. Move a patient from ER");
+		int x = s.nextInt();
+		if(x==1) {
+			System.out.println("Enter the room the patient is currently in:");
+			moveToSurgery(r[s.nextInt()], r[8]);
+		}else if(x==2) {
+			removeFromSurgery(r[8]);
+		}else if(x==3) {
+			System.out.println("Enter the new amount of time to stay in the hospital:");
+			r[9].patient.timeStay=s.nextInt();
+			reserveRoom(r,moveFromER(r[9]));
+		}else {
+			System.out.println("Input number 1, 2, or 3");
+		}
+	}//end movement
+	
 	public static void moveToSurgery(room r) {
-		
+		if(s.isOccupied==true) {
+			System.out.println("Unable to move at this time. The room is occupied by "+ s.getPatient()+".");
+		}else {
+			patient temp = r.getPatient();
+			s.patient=temp;
+			s.patient.isCritical=true;
+			s.isOccupied=true;
+		}
 	}//end surgery
 	
 	public static void removeFromSurgery(room r) {
-		
+		r.patient=null;
+		r.isOccupied=false;
 	}//end remove from surgery
 	
 	public static void moveFromER(room r) {
-		
+		patient temp = r.getPatient();
+		r.patient=null;
+		temp.isCritical=false;
+		r.isOccupied=false;
+		return temp;
 	}//end move from er
 }//end class
